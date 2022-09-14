244fcc2df0fdd5fb453b1509c2db0fec7ade7bc3
Victor Chavarria <victorm.chavarria@yahoo.com.mx>
2022-09-13 19:42:23 -0500

    .

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("Hello World...");
+
+
+class League {
+
+    constructor(name, teams=[], rounds=1) {
+        this.name = name
+        this.teams = teams
+        this.rounds = rounds
+        this.matchDaySchedule = []
+    }
+}
+
+const premierLeagueTeams = ['Chelsea', 'Arsenal'];
+
+const premier = new League('Premier League', premierLeagueTeams);
+
+for (const team of premier.teams) {
+    console.log(team)
+}
