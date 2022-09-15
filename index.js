c46f3dcb6ab29a5f9f57ed4e691b2622eb8fc79a
Victor Chavarria <victorm.chavarria@yahoo.com.mx>
2022-09-14 21:42:05 -0500

    Implementamos la clase PointsBasedLeague

--- index.js
+++ index.js
@@ -10,10 +10,19 @@ class League {
     }
 }
 
+class PointsBasedLeague extends League {
+    constructor(name, teams=[], rounds=1, pointsPerWin=3, pointsPerDraw=1, pointsPerLose=0) {
+        super(name, teams, rounds)
+        this.pointsPerWin = pointsPerWin
+        this.pointsPerDraw = pointsPerDraw
+        this.pointsPerLose = pointsPerLose
+    }
+}
+
 const premierLeagueTeams = ['Chelsea', 'Arsenal'];
 
-const premier = new League('Premier League', premierLeagueTeams);
-
+const premier = new PointsBasedLeague('Premier League', premierLeagueTeams);
+console.log(premier);
 for (const team of premier.teams) {
     console.log(team)
 }
