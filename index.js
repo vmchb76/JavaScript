fd7baf69de25a2a77c33cf04982e6ec256ac27e6
Victor Chavarria <victorm.chavarria@yahoo.com.mx>
2022-09-17 12:29:01 -0500

    Creamos el metodo setup para almacenar la configuracion de la liga

--- index.js
+++ index.js
@@ -2,27 +2,44 @@
 
 class League {
 
-    constructor(name, teams=[], rounds=1) {
+    constructor(name, teams=[], config={}) {
         this.name = name
         this.teams = teams
-        this.rounds = rounds
         this.matchDaySchedule = []
+        this.setup(config) 
+        
+    }
+
+    setup(config) { 
+        const defaultConfig = { rounds: 1}
+        this.config = Object.assign(defaultConfig, config)
+        
     }
 }
 
 class PointsBasedLeague extends League {
-    constructor(name, teams=[], rounds=1, pointsPerWin=3, pointsPerDraw=1, pointsPerLose=0) {
-        super(name, teams, rounds)
-        this.pointsPerWin = pointsPerWin
-        this.pointsPerDraw = pointsPerDraw
-        this.pointsPerLose = pointsPerLose
+    constructor(name, teams=[], config={} ) {
+        super(name, teams, config) 
     }
+      
+    setup(config) {
+        const defaultConfig = {
+            rounds : 1,
+            pointsPerWin: 3,
+            pointsPerDraw: 1,
+            pointsPerLose: 0
+        }
+        this.config = Object.assign(defaultConfig, config)
+    }
+
 }
 
 const premierLeagueTeams = ['Chelsea', 'Arsenal'];
 
-const premier = new PointsBasedLeague('Premier League', premierLeagueTeams);
-console.log(premier);
+const config = { rounds : 2, pointsPerWin: 3}
+const premier = new PointsBasedLeague('Premier League', premierLeagueTeams, config);
+
+console.log('CONFIG' ,premier.config);
 for (const team of premier.teams) {
     console.log(team)
 }
