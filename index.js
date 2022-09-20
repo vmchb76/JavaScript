596c4b030abe5e6c825b95758fe6cf869f9b6de6
Victor Chavarria <victorm.chavarria@yahoo.com.mx>
2022-09-19 21:39:39 -0500

    Usamos el operador spread para personalizar los equipos de la liga basada en puntos

--- index.js
+++ index.js
@@ -54,6 +54,18 @@ class PointsBasedLeague extends League {
         // Devolver un objeto con los mismos datos del objeto que devuelve el padre
         // Y ademas las propiedades: goalsFor: 0 y goalsAgainst :0
         // Tip: usar spreading
+        const customizedTeam = super.customizeTeam(teamName)
+        //customizedTeam.points = 0
+        //customizedTeam.goalsFor = 0
+        //customizedTeam.goalsAgainst = 0
+
+        //return customizedTeam
+        return {
+            points: 0,
+            goalsFor: 0,
+            goalsAgainst: 0,
+            ...customizedTeam
+        }
     }
 
 }
